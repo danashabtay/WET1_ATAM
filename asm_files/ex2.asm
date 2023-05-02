05f62ca10ef518241111f7c8de080e6c6b05a242
danashabtay <99danashabtay@gmail.com>
2023-05-02 16:47:44 +0300

    Update ex2.asm

--- asm_files/ex2.asm
+++ asm_files/ex2.asm
@@ -3,31 +3,6 @@
 .section .text
 _start:
 
-mov $0x0, %rcx
-mov $0x0, %rbx
-mov num, %ebx
-movsxd %ebx, %rbx
-cmp $0x0, %rbx
-jl NEGATIVE_HW1
-
-mov source, %rax
-mov destination, %rdx
-
-LOOP_HW1:
-	
-	cmp $0x0, %rbx
-	je END_HW1 
-
-	cmp %rbx, %rcx
-	jg END_HW1
-	
-	movb (%rax, %rcx, 1), %al
-	movb %al, (%rdx, %rcx, 1)	
-	
-	inc %rcx	
-	jmp LOOP_HW1
-	
-NEGATIVE_HW1:
-	mov %rbx, %rdx
-
-END_HW1:
+mov num, %eax
+movsx %eax, %rax
+mov %rax, destination

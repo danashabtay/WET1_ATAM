41bd7fe20ed0e798fffb7f0c341ee018267702dc
danashabtay <99danashabtay@gmail.com>
2023-05-02 15:20:33 +0300

    Update ex2.asm

--- asm_files/ex2.asm
+++ asm_files/ex2.asm
@@ -6,12 +6,12 @@ _start:
 movslq num, %rbx
 cmp $0x0, %rbx
 jg NEGATIVE_HW1
-mov $0x0, &rcx
+mov $0x0, %rcx
 
 LOOP_HW1:
 	cmp %rbx, %rcx
 	jg END_HW1
-	movb ((source),%rcx, $0x1), (destination)	
+	movb ((source), %rcx, $0x1), (destination)	
 	inc %rcx	
 	jmp LOOP_HW1
 	

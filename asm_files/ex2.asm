2a8f5a04e848a0732136f841daec50227bea58e8
danashabtay <99danashabtay@gmail.com>
2023-05-02 15:32:57 +0300

    Update ex2.asm

--- asm_files/ex2.asm
+++ asm_files/ex2.asm
@@ -5,12 +5,17 @@ _start:
 
 movslq num, %rbx
 cmp $0x0, %rbx
-jg NEGATIVE_HW1
+jl NEGATIVE_HW1
 mov $0x0, %rcx
 
 LOOP_HW1:
+	
+	cmp $0x0, %rbx
+	je END_HW1 
+
 	cmp %rbx, %rcx
 	jg END_HW1
+	
 	movb ((source), %rcx, $0x1), (destination)	
 	inc %rcx	
 	jmp LOOP_HW1
@@ -19,5 +24,3 @@ NEGATIVE_HW1:
 	movsq %rbx, (destination)
 
 END_HW1:
-
-#your code here

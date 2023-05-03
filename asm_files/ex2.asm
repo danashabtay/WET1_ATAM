5b53b64eb571cd62bf813148d9e02bc5a690dada
danashabtay <99danashabtay@gmail.com>
2023-05-03 13:48:54 +0300

    Update ex2.asm

--- asm_files/ex2.asm
+++ asm_files/ex2.asm
@@ -6,7 +6,7 @@ _start:
 mov $0x0, %rdx
 movl num, %eax
 movslq %eax, %rax
-mov $destination, %rbx
+
 mov $source, %rcx
 
 cmp $0x0, %rax
@@ -21,27 +21,29 @@ LOOP1_HW1:
 	cmp %rax, %rdx
 	jg LOOP2_HW1
 	
-	movb ((%rcx), %rdx, 1), %al
-	movb %al, ((%rbp), %rdx, 1)	
+	movb (%rcx, %rdx, 1), %rbx
+	movb (%rbx), (%rbp, %rdx, 1)	
 	
 	inc %rdx	
 	jmp LOOP1_HW1
 
 
 mov $0x0, %rdx
+mov $destination, %rbx 
 
 LOOP2_HW1: 
 
 	cmp %rax, %rdx
 	jg END_HW1
 	
-	movb ((%rbp), %rdx, 1), %al
-	movb %al, ((%rbx), %rdx, 1)	
+	movb (%rbp, %rdx, 1), %rcx
+	movb (%rcx), (%rbx, %rdx, 1)	
 	
 	inc %rdx	
 	jmp LOOP2_HW1
 
 NEGATIVE_HW1:
+	mov $destination, %rbx 
 	mov %rax, (%rbx)
 
 END_HW1:

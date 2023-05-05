c6347a32ea39b63d1a39e16bc1dee99c234d38e1
hofitf <hofit9@gmail.com>
2023-05-05 15:21:06 +0300

    Update ex2.asm

--- asm_files/ex2.asm
+++ asm_files/ex2.asm
@@ -3,47 +3,28 @@
 .section .text
 _start:
 
-mov $0x0, %edx
-movl num, %eax
+movl $0x0, %edx
+movl (num), %eax
 
-mov $source, %rcx
-
-cmp $0x0, %eax
+movq $source, %r8
+movq $destination, %rbx 
+cmpl $0x0, %eax
 jl NEGATIVE_HW1
 
 
 LOOP1_HW1:
-	
-	cmp $0x0, %eax
-	je END_HW1 
 
-	cmp %eax, %edx
-	jg LOOP2_HW1
+	cmpl %eax, %edx
+	jge END_HW1
 	
-	movq (%rcx, %edx, 1), %rbx
-	movb (%rbx), %bl
-	movb %bl, (%r9, %edx, 1)
+	movq (%r8, %rdx, 1), %r9
+	movb (%r9), %cl
+	movb %cl, (%rbx, %rdx, 1)
 	
 	incl %edx	
 	jmp LOOP1_HW1
 
-
-mov $0x0, %edx
-mov $destination, %rbx 
-
-LOOP2_HW1: 
-
-	cmp %eax, %edx
-	jg END_HW1
-	
-	movq (%r9, %edx, 1), %rcx
-	movb (%rcx), %cl	
-	movb %cl, (%rbx, %edx, 1)	
-	
-	incl %edx	
-	jmp LOOP2_HW1
-
 NEGATIVE_HW1:
-	movl %eax, destination
+	movl %eax, (destination)
 
 END_HW1:

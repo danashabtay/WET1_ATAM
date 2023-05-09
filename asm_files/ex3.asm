e7dfab6f51ca7646f00763a2a565f74536cefe96
danashabtay <99danashabtay@gmail.com>
2023-05-09 19:04:33 +0300

    Update ex3.asm

--- asm_files/ex3.asm
+++ asm_files/ex3.asm
@@ -6,8 +6,9 @@ _start:
 movl $array1, %eax
 movl $array2, %ebx
 movl $mergedArray, %ecx
+movl $0x0, (%ecx)
 
-MAIN_LOOP_HW1:
+CHECK_LOOP_HW1:
 	movl (%ecx), %r8d
 	movl (%eax), %r9d
 	movl (%ebx), %r10d
@@ -19,39 +20,35 @@ MAIN_LOOP_HW1:
 
 LOOP1_HW1:
 	cmp %r8d, %r9d 
-	je MAIN_LOOP_HW1
+	je CHECK_LOOP_HW1
 	
 	movl %r9d, (%ecx)
-	movl (%ecx), %r8d
 	addl $4, %ecx
 	addl $4, %eax
-	
-	jmp MAIN_LOOP_HW1
+	jmp CHECK_LOOP_HW1
 
 LOOP2_HW1:
 	cmp %r8d, %r10d 
-	je MAIN_LOOP_HW1
-	movl %r10d, (%ecx)
-	movl (%ecx), %r8d
-	addl $4, %ecx
-	addl $4, %ebx
+	je CHECK_LOOP_HW1
 	
-	jmp MAIN_LOOP_HW1
+	movl %r10d, (%ecx)
+	addl $4, %ecx
+	addl $4, %ebx	
+	jmp CHECK_LOOP_HW1
 
 LOOP_EQUAL_HW1:
 	cmp $0x0, %r9d
 	je END_HW1
 	
 	cmp %r8d, %r9d
-	je MAIN_LOOP_HW1
+	je CHECK_LOOP_HW1
 	
 	movl %r9d, (%ecx)
-	movl (%ecx), %r8d
 	addl $4, %ecx
 	addl $4, %eax
 	addl $4, %ebx
 	
-	jmp MAIN_LOOP_HW1
+	jmp CHECK_LOOP_HW1
 	
 END_HW1:
 	movl $0x0, (%ecx)

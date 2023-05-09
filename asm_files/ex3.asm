1e75a1ee85293e1be18ba3fe5a0e12ce257f8668
danashabtay <99danashabtay@gmail.com>
2023-05-09 18:41:36 +0300

    Update ex3.asm

--- asm_files/ex3.asm
+++ asm_files/ex3.asm
@@ -3,49 +3,49 @@
 .section .text
 _start:
 
-movl $array1, %rax
-movl $array2, %rbx
-movl $mergedArray, %rcx
+movl $array1, %eax
+movl $array2, %ebx
+movl $mergedArray, %ecx
 
 MAIN_LOOP_HW1:
-	movl (%rcx), %r8
-	movl (%rax), %r9
-	movl (%rbx), %r10
+	movl (%ecx), %r8d
+	movl (%eax), %r9d
+	movl (%ebx), %r10d
 	
-	cmp %r9, %r10 
+	cmp %r9d, %r10d 
 	jb LOOP1_HW1
 	ja LOOP2_HW1
 	je LOOP_EQUAL_HW1
 
 LOOP1_HW1:
-	cmp %r8, %r9 
+	cmp %r8d, %r9d 
 	je MAIN_LOOP_HW1
 	
-	movl %r9, (%rcx)
-	movl (%rcx), %r8
-	addl $4, %rcx
-	addl $4, %rax
+	movl %r9d, (%ecx)
+	movl (%ecx), %r8d
+	addl $4, %ecx
+	addl $4, %eax
 
 LOOP2_HW1:
-	cmp %r8, %r10 
+	cmp %r8d, %r10d 
 	je MAIN_LOOP_HW1
-	movl %r10, (%rcx)
-	movl (%rcx), %r8
-	addl $4, %rcx
-	addl $4, %rbx
+	movl %r10d, (%ecx)
+	movl (%ecx), %r8d
+	addl $4, %ecx
+	addl $4, %ebx
 
 LOOP_EQUAL_HW1:
-	cmp $0x0, %r9
+	cmp $0x0, %r9d
 	je END_HW1
 	
-	cmp %r8, %r9
+	cmp %r8d, %r9d
 	je MAIN_LOOP_HW1
 	
-	movl %r9, (%rcx)
-	movl (%rcx), %r8
-	addl $4, %rcx
-	addl $4, %rax
-	addl $4, %rbx
+	movl %r9d, (%ecx)
+	movl (%ecx), %r8d
+	addl $4, %ecx
+	addl $4, %eax
+	addl $4, %ebx
 	
 END_HW1:
-	movl $0x0, (%rcx)
+	movl $0x0, (%ecx)

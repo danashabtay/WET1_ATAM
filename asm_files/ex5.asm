7e5e44ea31ddc1e2c0d85c73a05978263e0e57c3
danashabtay <99danashabtay@gmail.com>
2023-05-10 01:58:11 +0300

    Update ex5.asm

--- asm_files/ex5.asm
+++ asm_files/ex5.asm
@@ -1,31 +1,46 @@
 .global _start
-    
+
 .section .text
 _start:
-   movq root, %rax #address of root
-    cmpq $0, %rax
-    jne search
+
+movq root, %rax
+movq new_node, %rdx
+
+testq %rax, %rax
+je EMPTY_TREE_HW1
+
+SEARCH_LOOP_W1:
+	cmpq new_node, (%rax)
+	je END_HW1
+
+	js RIGHT_NODE_HW1	
+	
+LEFT_NODE_HW1:
+	movq 8(%rax), %rbx
+	movq $8, %rsi
+	cmp $0, %rbx
+	je ADD_NODE_HW1
+	
+	movq 8(%rax), %rax
+	jmp SEARCH_LOOP_W1
+	
+RIGHT_NODE_HW1:
+	movq 16(%rax), %rbx
+	movq $16, %rsi
+	cmp $0, %rbx
+	je ADD_NODE_HW1
+	
+	movq 16(%rax), %rax
+	jmp SEARCH_LOOP_W1
+
+EMPTY_TREE_HW1:
     movq $new_node, root
-    jmp end
-search:
-    movq (%rax), %rbx #data in current node
-    movq %rax, %rcx
-    cmpq new_node, %rbx
-    je end
-    js right_tree
-left_tree:
-    movq 8(%rax), %rax
-    movq $8, %rsi
-    cmpq $0, %rax
-    je insert
-    jmp search
-right_tree:
-    movq 16(%rax), %rax
-    movq $16, %rsi
-    cmpq $0, %rax
-    je insert
-    jmp search
-insert:
-    movq $new_node, %r9
-    movq %r9, (%rcx,%rsi)
-end:
+	jmp END_HW1
+
+ADD_NODE_HW1:
+	movq $new_node, %r9
+	movq %r9, (%rax,%rsi)
+
+END_HW1:
+
+

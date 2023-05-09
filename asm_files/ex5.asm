f3e62d6abed9f7c55a5ffb10e25b24fd1dec5f90
danashabtay <99danashabtay@gmail.com>
2023-05-09 22:41:06 +0300

    Update ex5.asm

--- asm_files/ex5.asm
+++ asm_files/ex5.asm
@@ -2,4 +2,41 @@
 
 .section .text
 _start:
-#your code here
+
+movq root, %rax
+movq new_node, %rdx
+
+testq %rax, %rax
+je END_HW1
+
+COMPARE_LOOP_W1:
+	cmpq (%rax), (%rdx)
+	je END_HW1
+	ja RIGHT_NODE_HW1
+	jb LEFT_NODE_HW1
+
+RIGHT_NODE_HW1:
+	movq 16(%rax), %rbx
+	movq (%rbx), %rcx
+	cmp $0, $rcx
+	je ADD_NODE_HW1
+	
+	movq 16(%rax), %rax
+	jmp COMPARE_LOOP_W1
+
+LEFT_NODE_HW1:
+	movq 8(%rax), %rbx
+	movq (%rbx), %rcx
+	cmp $0, $rcx
+	je ADD_NODE_HW1
+	
+	movq 8(%rax), %rax
+	jmp COMPARE_LOOP_W1
+
+
+ADD_NODE_HW1:
+	movq %rdx, %rbx
+
+END_HW1:
+
+

b6227adf7f74210e2939e8e594aba9fd7ada04e2
danashabtay <99danashabtay@gmail.com>
2023-05-10 01:51:20 +0300

    Update ex4.asm

--- asm_files/ex4.asm
+++ asm_files/ex4.asm
@@ -9,14 +9,14 @@ movq Source, %rcx
 movl (%ecx), %r8d
 
 FIND_VAL_HW1:
-	testl %rdi, %rdi
+	testq %rdi, %rdi
 	je END_HW1
 
 	cmpl (%rdi), %ebx
 	je FIND_SOURCE_HW1
 	
-	movl %rdi, %rdx #SAVE ADDRESS OF NODE BEFORE VAL TO %RDX
-	addl $8, %rdi
+	movq %rdi, %rdx #SAVE ADDRESS OF NODE BEFORE VAL TO %RDX
+	addq $8, %rdi
 	jmp FIND_VAL_HW1 
 	
 FIND_SOURCE_HW1:
@@ -27,8 +27,8 @@ FIND_SOURCE_HW1:
 		cmpl (%rdi), %r8d
 		je SWITCH_HW1 #ADDRESS OF SOURCE IS SAVED IN %RDI
 		
-		movl %rdi, %r9 #SAVE ADDRESS OF NODE BEFORE SOURCE TO %R9
-		addl $8, %rdi
+		movq %rdi, %r9 #SAVE ADDRESS OF NODE BEFORE SOURCE TO %R9
+		addq $8, %rdi
 		jmp SOURCE_LOOP_HW1 
 
 

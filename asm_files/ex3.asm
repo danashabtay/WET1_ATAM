8262a4a0dd174a6e849830d4092a476226f360a4
hofitf <hofit9@gmail.com>
2023-05-10 15:09:08 +0300

    Update ex3.asm

--- asm_files/ex3.asm
+++ asm_files/ex3.asm
@@ -3,14 +3,17 @@
 .section .text
 _start:
 
-movl $array1, %eax
-movl $array2, %ebx
-movl $mergedArray, %ecx
+xor %rax, %rax #added
+xor %rbx, %rbx #added
+xor %rcx, %rcx #added
+movq $array1, %rax #e
+movq $array2, %rbx #e
+movq $mergedArray, %rcx #e
 movl $0x0, %edx
 
 CHECK_LOOP_HW1:
-	movl (%eax), %r9d
-	movl (%ebx), %r10d
+	movl (%rax), %r9d #e
+	movl (%rbx), %r10d #e
 	
 	cmp %r9d, %r10d 
 	jb COPY_LOOP1_HW1
@@ -21,29 +24,29 @@ COPY_LOOP1_HW1:
 	cmp %edx, %r9d
 	je DUP1_LOOP_HW1
 	
-	movl %r9d, (%ecx)
+	movl %r9d, (%rcx) #e
 	movl %r9d, %edx
-	addl $4, %ecx
-	addl $4, %eax
+	addq $4, %rcx #e
+	addq $4, %rax #e
 	jmp CHECK_LOOP_HW1
 
 COPY_LOOP2_HW1:
 	cmp %edx, %r10d
 	je DUP2_LOOP_HW1
 
-	movl %r10d, (%ecx)
+	movl %r10d, (%rcx) #e
 	movl %r10d, %edx
-	addl $4, %ecx
-	addl $4, %ebx	
+	addq $4, %rcx #e
+	addq $4, %rbx #e
 	jmp CHECK_LOOP_HW1
 	
 	
 DUP1_LOOP_HW1:
-	addl $4, %eax
+	addq $4, %rax #e
 	jmp CHECK_LOOP_HW1
 	
 DUP2_LOOP_HW1:
-	addl $4, %ebx
+	addq $4, %rbx #e
 	jmp CHECK_LOOP_HW1
 
 

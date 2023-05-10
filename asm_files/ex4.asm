1e42312fc5ba22f4453a0ea8feeb1c555c0202af
danashabtay <99danashabtay@gmail.com>
2023-05-10 22:17:58 +0300

    Update ex4.asm

--- asm_files/ex4.asm
+++ asm_files/ex4.asm
@@ -3,95 +3,127 @@
 .section .text
 _start:
 
-movq head, %rdi
-cmpq $0, %rdi
-je END_HW1
+movq head, %r9
+movq Source, %r8
 
-movq Source, %rcx
-cmpq $0, %rcx
-je END_HW1
-movl $Value, %ebx
-movl (%rcx), %edx
-cmpl %edx, %ebx
-je END_HW1
+cmpq $0, %r8
+jmp END_HW1
 
-movq $0, %r8 #previous node to value
-movq $0, %r9 #previous node to source
+cmpq $0, %r9
+jmp END_HW1
 
-SEARCH_VALUE_HW1: #find value and it's prev node
-	movq %rdi, %r10 #val
-	cmpl (%rdi), %ebx #check if node==val
-	je SEARCH_SOURCE_HW1 #if value is in the middle
+compq %r8, %r9
+je SOURCE_HEAD_HW1
+
+movl $Value, %r10
+cmpl (%r9), %r10
+je VALUE_HEAD_HW1
+
+jmp BOTH_MIDDLE_HW1
+
+SOURCE_HEAD_HW1: #r9=r8=head=src  
+	movl $Value, %r10
+	cmpl (%r9), %r10
+	je END_HW1
 	
-	cmpq $0, 4(%rdi) #check if next node is null
-    je END_HW1
-	
-	movq %rdi, %r8 #save previous to val in %r8
-	movq 4(%rdi), %rdi
-	jmp SEARCH_VALUE_HW1
+	VALUE_LOOP:
+		cmpl (%r9), %r10
+		je SWAP_SRC_FIRST_HW1
+		movq %r9, %r11 # %r11 = pre_val
+		movq 4(%r9), %r9
+		jmp VALUE_LOOP 
 
-SEARCH_SOURCE_HW1:
-	movq head, %rdi
-	movl (%rcx), %edx 
-	SOURCE_LOOP_HW1:
-		cmpq %rcx, %rdi #check if node==src
-		je SWITCH_HW1
-		
-		movq %rdi, %r9 #save previous to source in %r9
+SWAP_SRC_FIRST_HW1: # %r11 = pre_val %r8=src %r9=val_node
+	cmpq %r11, %r8
+	je ADJ_1
+	movq 4(%r9), %rbx # %rbx=val_next
+	movq 4(%r8), %rax # %rax=src_next
+	
+	movq %rax, 4(%r9)
+	movq %rbx, 4(%r8)
+	movq %r8, 4(%r11)
+	movq %r9, head
+	
+ADJ_1:
+	movq 4(%r9), %rbx # %rbx=val_next
+	movq %rbx, 4(%r8)
+	movq %r8, 4(%r9)
+	movq %r9, head
+
+VALUE_HEAD_HW1: #r9=head=val_node %r8=src
+	movq %r9, %rdi
+	cmpq %rdi, %r8
+	je END_HW1
+	
+	SRC_LOOP:
+		cmpq %rdi, %r8
+		je SWAP_VAL_FIRST_HW1
+		movq %rdi, %r12 # %r12 = pre_src
 		movq 4(%rdi), %rdi
-		jmp SOURCE_LOOP_HW1		
+		jmp SRC_LOOP 
+	
+SWAP_VAL_FIRST_HW1: #r9=head=val_node %r8=src %r12=pre_src
+	cmpq %r12, %r9
+	je ADJ_2_HW1
+	movq 4(%r9), %rbx # %rbx=val_next
+	movq 4(%r8), %rax # %rax=src_next
+	
+	movq %rax, 4(%r9)
+	movq %rbx, 4(%r8)
+	movq %r9, 4(%r12)
+	movq %r8, head
+	
+ADJ_2_HW1:
+	movq 4(%r8), %rax # %rax=src_next
+	movq %rax, 4(%r9)
+	movq %r9), 4(%r8)
+	movq %r8, head
 
-SWITCH_HW1: 
-#r10=val rdi=src r8=preval r9=presrc
-
-	movq 4(%rdi), %rax #save src_next in %rax
-	movq 4(%r10), %rbx #save val_next in %rbx
-	cmpq %rbx, %rdi
-	je ADJ_VAL_SRC_HW1
-	cmpq %rax, %r10
-	je ADJ_SRC_VAL_HW1
-	movq %rax, 4(%r10)
-	movq %rbx, 4(%rdi)
-	jmp SET_PRE_HW1
+BOTH_MIDDLE_HW1:
+	movq %r9, %rdi
+	FIND_VAL_HW1:
+		cmpl (%r9), %r10	
+		je FIND_SRC_HW1
+		cmpq $0, $r9 #val not found
+		jmp END_HW1
+		movq %r9, %r11 # %r11=preval
+		movq 4(%r9), %r9
+		jmp FIND_VAL_HW1
+		
+FIND_SRC_HW1: %r8=src %rdi=head %r11=preval %r9=val_node
+	cmpq %r8, %rdi
+	je SWAP2_HW1
+	movq %rdi, %r12 # %r12=presrc
+	movq 4(%rdi), %rdi
+	jmp FIND_SRC_HW1
 	
-ADJ_VAL_SRC_HW1:
-	movq %rax, 4(%r10)
-	movq %r10, 4(%rdi)
-	cmpq $0, %r8
-    je NO_PREVAL_HW1
-	movq %rdi, 4(%r8)
+SWAP2_HW1:	%r8=src %r12=presrc %r11=preval %r9=val_node
+ cmpq %r12, %r9
+ je ADJ_VAL_SRC_HW1
+ cmpq %r11, %r8
+ je ADJ_SRC_VAL_HW1
+ 
+ movq 4(%r8), %rax # %rax=src_next
+ movq 4(%r9), %rbx # %rbx=val_next
+ movq %r8, 4(%r11)
+ movq %rax, 4(%r9)
+ movq %r9, 4(%r12)
+ movq %rbx, 4(%r8)
+ jmp END_HW1
+ 
+ ADJ_VAL_SRC_HW1:
+	movq 4(%r8), %rax # %rax=src_next
+	movq %r8, 4(%r11)
+	movq %r9, 4(r8)
+	movq %rax, 4(%r9)
+	jmp END_HW1
+	
+ADJ_SRC_VAL_HW1:
+	movq 4(%r9), %rbx # %rbx=val_next
+	movq %r9, 4(%r12)
+	movq %r8, 4(r9)
+	movq %rax, 4(%r8)
 	jmp END_HW1
 
-	
-ADJ_SRC_VAL_HW1:	
-	movq %rbx, 4(%rdi)
-	movq %rdi, 4(%r10)
-	jmp SET_PRE_HW1
-	cmpq $0, %r9
-    je NO_PRESRC_HW1
-	movq %r10, 4(%r9)
-	jmp END_HW1
-	
-SET_PRE_HW1:
-	cmpq $0, %r8
-    je NO_PREVAL_HW1
-	movq %rdi, 4(%r8)
-	cmpq $0, %r9
-    je NO_PRESRC_HW1
-	movq %r10, 4(%r9)
-	jmp END_HW1	
-	
-NO_PREVAL_HW1:
-	movq %r10, 4(%r9)
-	cmpq %r10, head
-	jne END_HW1
-	movq %rdi, head
-	jmp END_HW1
-	
-NO_PRESRC_HW1: 	
-	cmpq %rdi, head
-	jne END_HW1
-	movq %r10, head
-	jmp END_HW1
-	
+
 END_HW1:
